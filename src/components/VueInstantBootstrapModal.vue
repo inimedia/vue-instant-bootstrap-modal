5fbe61978ddbe65e85dfcf93906842a2dcf4fa01
Adhe Widianjaya <adwidianjaya@gmail.com>
2017-05-21 22:54:50 +0700

    close on esc and backdrop click

--- src/components/VueInstantBootstrapModal.vue
+++ src/components/VueInstantBootstrapModal.vue
@@ -1,100 +1,121 @@
 <template>
-    <!-- container -->
-    <transition name="modal" v-if="showModal">
-        <div class="modal-mask" v-on:click="showModal=false">
-            <div class="modal-dialog" :class="[ 'modal-'+size ]">
-                <div class="modal-content" v-on:click.stop="">
-                    <slot></slot>
-                </div>
-                <!-- /.modal-content -->
-            </div>
-            <!-- /.modal-dialog -->
+  <!-- container -->
+  <transition name="modal" v-if="showModal">
+    <div class="modal-mask"
+         v-on:click="onModalMaskClicked()">
+      <div class="modal-dialog" :class="[ 'modal-'+size ]">
+        <div class="modal-content" v-on:click.stop="">
+          <slot></slot>
         </div>
-        <!-- /.modal -->
-    </transition>
+        <!-- /.modal-content -->
+      </div>
+      <!-- /.modal-dialog -->
+    </div>
+    <!-- /.modal -->
+  </transition>
 </template>
 <script>
-export default {
-  mounted () {
-    // console.log('IniBootstrapModal ready!', this.size);
-  },
-  props: {
-    size: {
-      type: String,
-      default: 'md'
-    }
-  },
-  data () {
-    var list = []
-    return {
-      notificationList: list,
-      showModal: false
-    }
-  },
-  methods: {
-    forceClose: function (notification) {
-      notification.forceKilled = true
-      this.sweep()
+  export default {
+    mounted () {
+      if (this.closeOnEsc) {
+        document.addEventListener("keydown", (e) => {
+          if (this.show && e.keyCode == 27) {
+            this.showModal = false;
+          }
+        });
+      }
     },
-    open: function () {
-      this.showModal = true
+    props: {
+      size: {
+        type: String,
+        default: 'md'
+      },
+      closeOnBackdrop: {
+        type: Boolean,
+        default: true
+      },
+      closeOnEsc: {
+        type: Boolean,
+        default: true
+      }
     },
-    show: function () {
-      this.showModal = true
+    data () {
+      var list = []
+      return {
+        notificationList: list,
+        showModal: false
+      }
     },
-    close: function () {
-      this.showModal = false
+    methods: {
+      onModalMaskClicked: function () {
+        if (!this.closeOnBackdrop) {
+          return;
+        }
+        this.showModal = false;
+      },
+      forceClose: function (notification) {
+        notification.forceKilled = true
+        this.sweep()
+      },
+      open: function () {
+        this.showModal = true
+      },
+      show: function () {
+        this.showModal = true
+      },
+      close: function () {
+        this.showModal = false
+      },
+      hide: function () {
+        this.showModal = false
+      }
     },
-    hide: function () {
-      this.showModal = false
-    }
-  },
-  filters: {
-    capitalize: function (str) {
-      return str.charAt(0).toUpperCase() + str.slice(1)
+    filters: {
+      capitalize: function (str) {
+        return str.charAt(0).toUpperCase() + str.slice(1)
+      }
     }
   }
-}
 </script>
 <style>
-.modal-mask {
-  position: fixed;
-  z-index: 9996;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, .2);
-  display: block;
-  -webkit-transition: opacity .3s ease-in-out;
-  -o-transition: opacity .3s ease-in-out;
-  transition: opacity .3s ease-in-out;
-  overflow-y: auto!important;
-}
+  .modal-mask {
+    position: fixed;
+    z-index: 9996;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, .2);
+    display: block;
+    -webkit-transition: opacity .3s ease-in-out;
+    -o-transition: opacity .3s ease-in-out;
+    transition: opacity .3s ease-in-out;
+    overflow-y: auto!important;
+  }
 
-.modal-content {
-  border: 0px;
-  border-radius: 2px;
-  -webkit-transition: transform .3s ease-in-out;
-  -o-transition: transform .3s ease-in-out;
-  transition: transform .3s ease-in-out;
-}
+  .modal-content {
+    border: 0px;
+    border-radius: 2px;
+    -webkit-transition: transform .3s ease-in-out;
+    -o-transition: transform .3s ease-in-out;
+    transition: transform .3s ease-in-out;
+  }
 
-.modal-enter {
-  opacity: 0;
-}
+  .modal-enter {
+    opacity: 0;
+  }
 
-.modal-leave-active {
-  opacity: 0;
-}
+  .modal-leave-active {
+    opacity: 0;
+  }
 
-.modal-enter .modal-content {
-  -webkit-transform: translate(0px, -20px);
-  transform: translate(0px, -20px);
-}
+  .modal-enter .modal-content {
+    -webkit-transform: translate(0px, -20px);
+    transform: translate(0px, -20px);
+  }
 
-.modal-leave-active .modal-content {
-  -webkit-transform: translate(0px, -20px);
-  transform: translate(0px, -20px);
-}
+  .modal-leave-active .modal-content {
+    -webkit-transform: translate(0px, -20px);
+    transform: translate(0px, -20px);
+  }
 </style>
